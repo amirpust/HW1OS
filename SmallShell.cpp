a821d2f710d8ae522649a21fb3f3898ed8b9d183
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2020-04-11 09:36:11 +0300

    split executeCommand (on smallShell): - builtin - exteral

--- SmallShell.cpp
+++ SmallShell.cpp
@@ -8,25 +8,33 @@
 #define MAX_ARGS 20
 
 void SmallShell::executeCommand(const char *cmd_line) {
-    //TODO :check if builtIn or external
     char* cmd_l = new char[strlen(cmd_line) + 1];
     strcpy(cmd_l,cmd_line);
-    if(!_isBackgroundComamnd(cmd_l)){
-        Command* cmd = CreateCommand(cmd_line);
+    bool onBG = !_isBackgroundComamnd(cmd_l);
+    _removeBackgroundSign(cmd_l);
+    Command* cmd = CreateCommand(cmd_line);
+
+    if(cmd->getType() == builtIn){
         cmd->execute();
-}
-    else{
-        _removeBackgroundSign(cmd_l);
-        Command* cmd = CreateCommand(cmd_line);
-        cmd->execute();
-        pid_t p = fork();
-        if(p == 0){
+    }else if(cmd->getType() == external){
+        pid_t pid = fork();
+        if(pid == 0){                                //Child
             cmd->execute();
-        }else{
-            jobs.addJob(cmd,p);
+            exit(-1); //we'll probably use execv and wont reach here.
+        }else{                                      //Parent
+            jobs.addJob(cmd,pid);
+            if(!onBG){
+                int status;
+                waitpid(pid, &status);
+                if(status == -1)            //TODO : handle exception
+                    exit(-1);
+                return;
+            }else{
+                return;
+            }
         }
     }
-    // Please note that you must fork smash process for some commands (e.g., external commands....)
+    //TODO: add more types
 }
 
 const std::string SmallShell::getName() const {
