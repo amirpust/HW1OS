312d0b300490ceae245efacdea31067442c26fab
amirpust <54210536+amirpust@users.noreply.github.com>
2020-04-14 18:45:12 +0300

    no splitting us up

--- signals.cpp
+++ signals.cpp
@@ -6,36 +6,15 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-    try{//TODO: make sendSig with Default id (the default will be taking the current fg task)
-        SmallShell::getInstance().getJobs().sendSigById(sig_num);
-    }catch (exception& e){
-        return;
-    }
-    /*if(SmallShell::getInstance().getJobs().getSize() == 0)
-        return;
 
-    SmallShell::getInstance().getJobs().getLastJob(NULL)->stopCmd();
-    kill(SmallShell::getInstance().getJobs().getLastJob()->getJobPid(), sig_num);
-     */
     cout << "smash: got ctrl-Z" << endl;
 
 }
 
 void ctrlCHandler(int sig_num) {
-    try{//TODO: make sendSig with Default id (the default will be taking the current fg task)
-        SmallShell::getInstance().getJobs().sendSigById(sig_num);
-    }catch (exception& e){
-        return;
-    }/*
-    if(SmallShell::getInstance().getJobs().getSize() == 0)
-        return;
-
-    pid_t pid = SmallShell::getInstance().getJobs().getLastJob()->getJobPid();
-    SmallShell::getInstance().getJobs().killCommand(
-            SmallShell::getInstance().getJobs().getLastJob(),false);*/
     cout<< "smash: got ctrl-C"<<endl;
     cout<< "smash: process " + std::to_string(getpid()) + " was killed"<<endl;
-
+    //exit(9);
 }
 
 void alarmHandler(int sig_num) {
