ef625a042f687a9ee523e6741d1232bb12211be7
amirpust <54210536+amirpust@users.noreply.github.com>
2020-04-14 17:51:24 +0300

    signals

--- signals.cpp
+++ signals.cpp
@@ -6,28 +6,41 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-    if(SmallShell::getInstance().getJobs().getSize() == 0)
+    try{//TODO: make sendSig with Default id (the default will be taking the current fg task)
+        SmallShell::getInstance().getJobs().sendSigById(sig_num);
+    }catch (exception& e){
+        return;
+    }
+    /*if(SmallShell::getInstance().getJobs().getSize() == 0)
         return;
 
     SmallShell::getInstance().getJobs().getLastJob(NULL)->stopCmd();
     kill(SmallShell::getInstance().getJobs().getLastJob()->getJobPid(), sig_num);
+     */
     cout << "smash: got ctrl-Z" << endl;
 
 }
 
 void ctrlCHandler(int sig_num) {
+    try{//TODO: make sendSig with Default id (the default will be taking the current fg task)
+        SmallShell::getInstance().getJobs().sendSigById(sig_num);
+    }catch (exception& e){
+        return;
+    }/*
     if(SmallShell::getInstance().getJobs().getSize() == 0)
         return;
 
     pid_t pid = SmallShell::getInstance().getJobs().getLastJob()->getJobPid();
     SmallShell::getInstance().getJobs().killCommand(
-            SmallShell::getInstance().getJobs().getLastJob(),false);
+            SmallShell::getInstance().getJobs().getLastJob(),false);*/
     cout<< "smash: got ctrl-C"<<endl;
     cout<< "smash: process " + std::to_string(pid) + " was killed"<<endl;
 
 }
 
 void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
+    cout << "smash got an alarm" <<  endl;
+    const char* cmd = SmallShell::getInstance();//TODO: get last command in the shell
+    cout << "smash: " + cmd + "timed out!"<< endl;
 }
 
