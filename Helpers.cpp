874210250b753a4d2da2e0afd260389c1297dc7a
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2020-04-16 11:25:37 +0300

    Signals: -fixed time bug

---
+++ Helpers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonat on 16/04/2020.
+//
+
+#include "Helpers.h"
---
+++ Helpers.h
@@ -0,0 +1,30 @@
+//
+// Created by jonat on 16/04/2020.
+//
+#ifndef HW1_HELPERS_H
+#define HW1_HELPERS_H
+#include "libs.h"
+
+using std::string;
+class Helpers {
+    static string charToStr(const char* source, string* dest){
+
+    }
+
+public:
+
+
+    static Command decryptCmdLine(const char* originalCmdLine){
+        string originalCommandLine;
+        string args[COMMAND_MAX_ARGS];
+        int size;
+        cmdType type;
+        string path;
+
+
+        return Command();
+    }
+};
+
+
+#endif //HW1_HELPERS_H
